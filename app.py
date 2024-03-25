834412d61b14e4514dcf258ffec2d7c9a668f6c8
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-25 09:53:41 -0700

    basic flask setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask # Import the Flask class from the flask module
+
+
+# Create an instance of Flask called app which will be the central object
+app = Flask(__name__)
+
+
+# Define a route
+@app.route("/")
+def index():
+    first_name = 'Brian'
+    age = 55
+    return 'Hello ' + first_name + ' who is ' + str(age) + ' years old'
+
