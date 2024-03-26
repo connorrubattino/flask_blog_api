55085b368be54f9018e949e669218c73ccbbdcb9
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-26 14:20:40 -0700

    add flask_migrate and first migreation with User model for user table

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask # Import the Flask class from the flask module
 from flask_sqlalchemy import SQLAlchemy #import database
+from flask_migrate import Migrate
 from config import Config
 
 
@@ -11,6 +12,8 @@ app.config.from_object(Config)
 
 #create an instance of SQLAlchemy called db which will be the central object for our database
 db = SQLAlchemy(app)
+# Create an instance of Migrate with the app and db
+migrate = Migrate(app, db)
 
-#import the routes to the app -- need this below the app or else will cause circular import because when it goes over to routes to look for app, app will not yet be defined
-from . import routes
+#import the routes and models to the app -- need this below the app or else will cause circular import because when it goes over to routes to look for app, app will not yet be defined
+from . import routes, models
