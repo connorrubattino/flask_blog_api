9a560055c8def3151e391aa43b7fe44fb76903f6
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-27 12:18:24 -0700

    added post class and post table plus migrations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db   #can also be from app import db
-from datetime import datetime
+from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
@@ -9,7 +9,7 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -39,3 +39,28 @@ class User(db.Model):
             "email": self.email,
             "dateCreated": self.date_created
         }
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    # In SQL - user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id) -- 'user.id' below takes id from user table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+                    #anytime the database models change (new tables or column) must rerun flask db migrate and upgrade
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Post {self.id}|{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+                
