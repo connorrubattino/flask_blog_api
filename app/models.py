effb78c6944337f890fe014acde6a7395e38c162
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-28 08:56:30 -0700

    setup token and token exp

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+import secrets
 from . import db   #can also be from app import db
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
@@ -10,8 +11,9 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-    posts = db.relationship('Post', back_populates='author') #matching inverse of below in post
-                    #above is connecting them in pyhton with relationship -- foriegn keys are for database
+    posts = db.relationship('Post', back_populates='author') #matching inverse of below in post is connecting them in pyhton with relationship -- foriegn keys are for database
+    token = db.Column(db.String, index=True, unique=True)
+    token_expiration = db.Column(db.DateTime(timezone=True))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -42,6 +44,14 @@ class User(db.Model):
             "dateCreated": self.date_created
         }
 
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "tokenExpiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_expiration}
 
 
 class Post(db.Model):
@@ -53,7 +63,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     author = db.relationship('User', back_populates='posts')  #matching inverse of up in User
 
-                    #anytime the database models change (new tables or column) must rerun flask db migrate and upgrade
+                    #anytime the database models change (new tables or column) must rerun flask db migrate -m""" and upgrade
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
