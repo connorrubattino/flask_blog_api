318f9d95b93322afde650636e87133887738bc22
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-17 12:40:04 -0700

    import CORS, added post to db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask # Import the Flask class from the flask module
 from flask_sqlalchemy import SQLAlchemy #import database
 from flask_migrate import Migrate
+from flask_cors import CORS #import cors to allow Cros Origin Resource Sharing
 from config import Config
 
 
@@ -9,6 +10,9 @@ app = Flask(__name__)
 
 #set the configuration for the app
 app.config.from_object(Config)
+
+#Allow cross Origin Resorce Sharing for all domains on all routes
+CORS(app)
 
 #create an instance of SQLAlchemy called db which will be the central object for our database
 db = SQLAlchemy(app)
